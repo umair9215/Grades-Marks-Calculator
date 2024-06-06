bc3381195bd1f31c9a2c12ec9a6ba8d4af1be357
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-06-06 11:11:31 -0700

    By Malik Muhammad Umair

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var a = prompt("Enter your marks out of 100");
+if (a >= 90 && a===100) {
+    alert("Your Grade is Grade A");
+}
+ else if (a >=80 && a <= 90) {
+    alert("Your Grade is Grade B");
+ }
+ else if (a >=70  &&  a <= 60) {
+    alert("Your Grade is Grade C");
+ }
+ else if (a >=60   &&  a <= 70) {
+    alert("Your Grade is Grade D");
+ }
+ else  {
+    alert("You Are FAil");
+ }
